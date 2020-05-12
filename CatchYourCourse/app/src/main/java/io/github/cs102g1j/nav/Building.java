5667086a5b1dae4bb53d6047cc9dea984372c9a1
MustafaUtkuAydogdu <63458049+MustafaUtkuAydogdu@users.noreply.github.com>
2020-05-12 18:32:41 +0300

    some of the classes we will use in the program. Will be updated after some reviews and solving minor issues.

--- CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/Building.java
+++ CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/Building.java
@@ -1,15 +1,98 @@
-/*
 package io.github.cs102g1j.nav;
 
-class Building
-{
-   // location of the Building
-   //Constructor
-   ( enlem, boylam);
-   ( String: googlemaps'ten yer linki) // uğraşmaya değer değil mi?
+import android.location.Location;
 
-   //Methods
-   getter/setter
-   lazım olabilecek diğerleri
+public class Building
+{
+   //constants locations
+   public final static Building BUILDING_EE = new Building(39.872094, 32.750913,"BUILDING_EE");
+   public final static Building BUILDING_EA = new Building(39.871195, 32.750058,"BUILDING_EA");
+   public final static Building BUILDING_S = new Building(39.867909, 32.748267,"BUILDING_S");
+   public final static Building BUILDING_B = new Building(39.868667, 32.748309,"BUILDING_B");
+   public final static Building BUILDING_V = new Building(39.867379025065425, 32.75014751011084,"BUILDING_V");
+
+
+   //properties
+   String nameOfBuilding;
+   double latitudeOfBuilding;
+   double longtitudeOfBuilding;
+
+
+
+   //constructors
+   public Building( double longtitude, double latitude,String name)
+   {
+      nameOfBuilding = name;
+      latitudeOfBuilding = latitude;
+      longtitudeOfBuilding = longtitude;
+
+   }
+
+   public Building ( Building building)
+   {
+      nameOfBuilding = building.getNameOfBuilding();
+      latitudeOfBuilding = building.getLatitudeOfBuilding();
+      longtitudeOfBuilding = building.getLongtitudeOfBuilding();
+
+   }
+
+
+
+   //methods
+   public String getNameOfBuilding()
+   {
+      return nameOfBuilding;
+   }
+
+   public double getLatitudeOfBuilding()
+   {
+      return latitudeOfBuilding;
+   }
+
+   public double getLongtitudeOfBuilding()
+   {
+      return longtitudeOfBuilding;
+   }
+
+   public double setLatitudeOfBuilding( double latitude)
+   {
+      latitudeOfBuilding = latitude;
+      return latitude;
+   }
+
+   public double setLongtitudeOfBuilding( double longtitude)
+   {
+      longtitudeOfBuilding = longtitude;
+      return longtitude;
+   }
+
+   private double getDistance(Location currentLocation ) {
+      double theta = longtitudeOfBuilding - currentLocation.getLongitude();
+      double dist = Math.sin(deg2rad(latitudeOfBuilding))
+                    * Math.sin(deg2rad(currentLocation.getLatitude()))
+                    + Math.cos(deg2rad(latitudeOfBuilding))
+                      * Math.cos(deg2rad(currentLocation.getLatitude()))
+                      * Math.cos(deg2rad(theta));
+      dist = Math.acos(dist);
+      dist = rad2deg(dist);
+      dist = dist * 60 * 1.1515;
+      return (dist);
+   }
+
+   private double deg2rad(double deg) {
+      return (deg * Math.PI / 180.0);
+   }
+
+   private double rad2deg(double rad) {
+      return (rad * 180.0 / Math.PI);
+   }
+
+   public boolean isNearer( double meter,Location currentLocation)
+   {
+      if (getDistance(currentLocation) <= meter/100)
+         return true;
+      else
+         return false;
+   }
+
 }
-*/
--- CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/MyClasses.java
+++ CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/MyClasses.java
@@ -1,24 +1,84 @@
-/*
 package io.github.cs102g1j.nav;
 
-class MyClasses
+
+import android.location.Location;
+
+import java.text.ParseException;
+
+import io.github.cs102g1j.nav.Building;
+
+public class MyClasses
 {
-   // public Constants
-   Buildings ( ayrı ayrı...)
-   Lectures ( ayrı ayrı...) //ders adları
+   //constants
+   public final String MATH_102 = "MATH_102";
+   public final String CS_102 =   "CS_102";
+   public final String ENG_102 = "ENG_102";
+   public final String MATH_132 = "MATH_132";
+   public final String TURK_102 = "TURK_102";
 
 
-   // properties
-   String lecture
-   Building build
-   MyDate date
 
-      //Constructor
-   ( MyDate, Building, lecture)
-	( MyDate, Building)
+   //properties
+   String lectureName;
+   Building lectureBuilding;
+   MyDate lectureTime;
 
-   //Methods
-   getter/setter
-   lazım olabilecek diğerleri
+
+   //constructors
+   public MyClasses( MyDate date, Building building, String lectureName)
+   {
+      lectureTime = date;
+      lectureBuilding = building;
+      this.lectureName = lectureName;
+
+   }
+
+
+   //methods
+   public String getLectureName()
+   {
+      return lectureName;
+   }
+
+   public Building getLectureBuilding()
+   {
+      return lectureBuilding;
+   }
+
+   public MyDate getLectureTime()
+   {
+      return lectureTime;
+   }
+
+   // SHOULD THE SETTERS ALSO RETURN THE  VALUES WE SET
+   public void setLectureName( String name)
+   {
+      lectureName = name;
+   }
+
+   public void setLectureBuilding( Building building)
+   {
+      lectureBuilding = building;
+
+   }
+
+   public void setLectureTime( MyDate date)
+   {
+      lectureTime = date;
+   }
+
+   /* WE CAN CHANGE THİS THE WAY BY ADDİNG SOME LOCATİON FEATURES OR GETTİNG CURRENT LOCATİON FROM LOCATİON CLASS.
+   public boolean isNow( Location currentLocation) throws ParseException {
+      if (lectureBuilding.isNearer(10,currentLocation) && lectureTime.isNow())
+         return true;
+      else return false;
+
+   }
+   *
+    */
+
+
+
+
+
 }
-*/
