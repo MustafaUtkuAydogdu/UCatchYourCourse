ecc28021be804290bfbde41c1b1e2c0dfceb433c
GirayAkyol <giray.akyol@ug.bilkent.edu.tr>
2020-05-19 17:02:45 +0300

    Testda

--- CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/MyDate.java
+++ CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/MyDate.java
@@ -25,11 +25,7 @@ public class MyDate
    //methods
    public boolean isIncludes( MyDate time)
    {
-      if ( startTime <= time.startTime && endTime >= time.endTime)
-      {
-         return true;
-      }
-      return false;
+      return startTime <= time.startTime && endTime >= time.endTime;
    }
 
    public int getStartTime()
@@ -69,7 +65,7 @@ public class MyDate
       else if ( day == 3 )
          dayName = "Tuesday";
       else if ( day == 4 )
-         dayName = "Wednasday";
+         dayName = "Wednesday";
       else if ( day == 5 )
          dayName = "Thursday";
       else if ( day == 6 )
