7a10fed947225dcd91e8e5c93636cd22e892a64b
GirayAkyol <giray.akyol@ug.bilkent.edu.tr>
2020-05-20 06:57:05 +0300

    dw

--- CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/Building.java
+++ CatchYourCourse/app/src/main/java/io/github/cs102g1j/nav/Building.java
@@ -1,10 +1,6 @@
 package io.github.cs102g1j.nav;
 /**
  * This is Building class.
- * @author Mustafa Yasir Altunhan.
- * @author Mustafa Utku Aydoğdu
- * @author Muhammed Can Küçükaslan
- * @version 19.05.2020
  */
 
 import android.location.Location;
@@ -14,11 +10,14 @@ import android.os.Parcelable;
 public class Building implements Parcelable
 {
    //constants locations
-   public final static Building BUILDING_EE = new Building(39.872094, 32.750913,"EE");
-   public final static Building BUILDING_EA = new Building(39.871195, 32.750058,"EA");
-   public final static Building BUILDING_S = new Building(39.867909, 32.748267,"S");
-   public final static Building BUILDING_B = new Building(39.868667, 32.748309,"B");
-   public final static Building BUILDING_MA = new Building(39.867379025065425, 32.75014751011084,"MA");
+   public final static Building BUILDING_EE = new Building( 39.872094, 32.750913, "EE" );
+   public final static Building BUILDING_EA = new Building( 39.871195, 32.750058, "EA" );
+   public final static Building BUILDING_S = new Building( 39.867909, 32.748267, "S" );
+   public final static Building BUILDING_B = new Building( 39.868667, 32.748309, "B" );
+   public final static Building BUILDING_MA = new Building( 39.867379025065425,
+                                                            32.75014751011084,
+                                                            "MA"
+   );
 
 
    //properties
@@ -27,9 +26,8 @@ public class Building implements Parcelable
    double longitudeOfBuilding;
 
 
-
    //constructors
-   public Building( double latitude, double longitude, String name)
+   public Building( double latitude, double longitude, String name )
    {
       nameOfBuilding = name;
       latitudeOfBuilding = latitude;
@@ -37,7 +35,7 @@ public class Building implements Parcelable
 
    }
 
-   public Building ( Building building)
+   public Building( Building building )
    {
       nameOfBuilding = building.getNameOfBuilding();
       latitudeOfBuilding = building.getLatitudeOfBuilding();
@@ -77,7 +75,7 @@ public class Building implements Parcelable
 
    // Creating a Building object invoking the Building( Parcel in) constructor.
    // Also creates an array of Buildings in case of using a Building array in program.
-   public static final Creator< Building > CREATOR = new Creator< Building >()
+   public static final Creator<Building> CREATOR = new Creator<Building>()
    {
       @Override
       public Building createFromParcel( Parcel in )
@@ -108,80 +106,81 @@ public class Building implements Parcelable
       return longitudeOfBuilding;
    }
 
-   public double setLatitudeOfBuilding( double latitude)
+   public double setLatitudeOfBuilding( double latitude )
    {
       latitudeOfBuilding = latitude;
       return latitude;
    }
 
-   public double setLongitudeOfBuilding( double longtitude)
+   public double setLongitudeOfBuilding( double longtitude )
    {
       longitudeOfBuilding = longtitude;
       return longtitude;
    }
 
-   /*
-    * This is the method that finds the distance between our currentLocation and the building object.
-    *@param currentLocation is the currentLocation of the user.
-    *@return returns the distance as an double value.
+   /**
+    * This is the method that finds the distance between our currentLocation and the building
+    * object.
+    *
+    * @param currentLocation is the currentLocation of the user.
+    *
+    * @return returns the distance as an double value.
     */
    private double getDistance( Location currentLocation )
    {
       double theta = longitudeOfBuilding - currentLocation.getLongitude();
-      double dist = Math.sin( deg2rad( latitudeOfBuilding))
-                    * Math.sin( deg2rad( currentLocation.getLatitude()))
-                    + Math.cos( deg2rad( latitudeOfBuilding))
-                      * Math.cos( deg2rad( currentLocation.getLatitude()))
-                      * Math.cos( deg2rad( theta));
-      dist = Math.acos( dist);
-      dist = rad2deg( dist);
+      double dist = Math.sin( deg2rad( latitudeOfBuilding ) ) *
+                    Math.sin( deg2rad( currentLocation.getLatitude() ) ) + Math.cos( deg2rad(
+         latitudeOfBuilding ) ) * Math.cos( deg2rad( currentLocation.getLatitude() ) ) * Math.cos(
+         deg2rad( theta ) );
+      dist = Math.acos( dist );
+      dist = rad2deg( dist );
       dist = dist * 60 * 1.1515;
-      return ( dist);
+      return ( dist );
    }
 
-   /*
-    * One of the methods that helps to getDistance method
-    * Turns the degree input to the radian.
-    *@param deg is the deg input.
+   /**
+    * One of the methods that helps to getDistance method Turns the degree input to the radian.
+    *
+    * @param deg is the deg input.
+    *
     * @return returns the radian value as a double.
     */
-   private double deg2rad( double deg)
+   private double deg2rad( double deg )
    {
-      return ( deg * Math.PI / 180.0);
+      return ( deg * Math.PI / 180.0 );
    }
 
-   /*
-    * One of the methods that helps to getDistance method
-    * Turns the radian input to the degree.
+   /**
+    * One of the methods that helps to getDistance method Turns the radian input to the degree.
+    *
     * @param rad is the rad input.
+    *
     * @return returns the degree value as a double.
     */
-   private double rad2deg( double rad)
+   private double rad2deg( double rad )
    {
-      return ( rad * 180.0 / Math.PI);
+      return ( rad * 180.0 / Math.PI );
    }
 
-   /*
-    * This is the method that checks whether we are close enough to the Building.
-    * Method returns true if the user is closer to the Building than specified distance,otherwise return false.
-    *@param meter is the specified distance.
+   /**
+    * This is the method that checks whether we are close enough to the Building. Method returns
+    * true if the user is closer to the Building than specified distance,otherwise return false.
+    *
+    * @param meter is the specified distance.
     * @param currentLocation is the current location of the user.
-    *@return returns the boolean value
+    *
+    * @return returns the boolean value
     */
-   public boolean isNearer( double meter, Location currentLocation)
+   public boolean isNearer( double meter, Location currentLocation )
    {
-      if ( getDistance( currentLocation) <= meter / 1000)
-      {
-         return true;
-      }
-      else
-         return false;
+      return getDistance( currentLocation ) <= meter / 1000;
    }
 
 
-
-   /*
+   /**
     * A toString method that returns the name of the building.
+    *
     * @return returns the name of the building as a String.
     */
    @Override
@@ -191,31 +190,32 @@ public class Building implements Parcelable
    }
 
 
-
-   /*
+   /**
     * This is the method that return a building object according to the user input in application.
-    *@param buildingCode is the input that users enter in the app when creating their schedule
-    *@return return the appropriate building, if input is invalid return null.
+    *
+    * @param buildingCode is the input that users enter in the app when creating their schedule
+    *
+    * @return return the appropriate building, if input is invalid return null.
     */
-   public static Building getBuilding( String buildingCode)
+   public static Building getBuilding( String buildingCode )
    {
-      if ( buildingCode. equals( "EE") )
+      if ( buildingCode.equals( "EE" ) )
       {
          return BUILDING_EE;
       }
-      else if (buildingCode.equals( "EA" ))
+      else if ( buildingCode.equals( "EA" ) )
       {
          return BUILDING_EA;
       }
-      else if (buildingCode.equals( "S" ))
+      else if ( buildingCode.equals( "S" ) )
       {
          return BUILDING_S;
       }
-      else if (buildingCode.equals( "B" ))
+      else if ( buildingCode.equals( "B" ) )
       {
          return BUILDING_B;
       }
-      else if (buildingCode.equals( "MA" ))
+      else if ( buildingCode.equals( "MA" ) )
       {
          return BUILDING_MA;
       }
